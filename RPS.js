26567fe9c784c4e4c3d55886c6601b64112fa6b6
Nicholas Petree <123441251+Pete1230z@users.noreply.github.com>
2023-04-29 20:03:02 -0500

    Create function to generate computer response

---
+++ RPS.js
@@ -0,0 +1,21 @@
+//Create function to generate computer program 
+
+function getComputerChoice() { 
+
+	const choices = ['Rock', 'Paper', 'Scissors'];
+	const randomChoice = Math.floor(Math.random() * choices.length);
+	const decision = choices[randomChoice];
+	return decision;
+
+ }
+
+ //Create a way for the user to input their answer
+
+ function playerDecision() {
+
+ }
+
+ //Create a comparison about which RPS beats what
+
+ //Create a way to say you won or you lost
+
