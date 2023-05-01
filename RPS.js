f093d24f8f04c6373f501776f72e4924f80b869d
Nicholas Petree <123441251+Pete1230z@users.noreply.github.com>
2023-04-30 21:26:32 -0500

    Create a single round

--- RPS.js
+++ RPS.js
@@ -1,23 +1,58 @@
-//Create function to generate computer program 
+
+let Humanity = 0;
+let AI = 0;
+
+//Create function to generate random computer choice 
 
 function getComputerChoice() { 
 
-	const choices = ['Rock', 'Paper', 'Scissors'];
+	const choices = ['rock', 'paper', 'scissors'];
 	const randomChoice = Math.floor(Math.random() * choices.length);
 	const decision = choices[randomChoice];
 	return decision;
 
  } getComputerChoice();
 
- //Create a way for the user to input their answer
+ //play a round of game
 
- function playerDecision() { 
-	const userInput = prompt('Make your decision');
-	return userInput;
 
- } playerDecision();
+ function round() {
 
- //Create a comparison about which RPS beats what
+	let computerChoice = getComputerChoice();
+    let playerDecision = prompt('Make your decision!', '').toLowerCase();
 
- //Create a way to say you won or you lost
+
+	if (playerDecision === 'rock' && computerChoice === 'scissors') {
+		Humanity += 1;
+		alert('Humanity Wins');
+	} else if (playerDecision === 'scissors' && computerChoice === 'paper') {
+		Humanity += 1;
+		alert('Humanity Wins');
+	} else if (playerDecision === 'paper' && computerChoice === 'rock') {
+		Humanity += 1;
+		alert('Humanity Wins');
+	} else if (playerDecision === 'rock' && computerChoice === 'paper') {
+		AI += 1;
+		alert('AI Wins');
+	} else if (playerDecision === 'scissors' && computerChoice === 'rock') {
+		AI += 1;
+		alert('AI Wins');
+	} else if (playerDecision === 'paper' && computerChoice === 'scissors') {
+		AI += 1;
+		alert('AI Wins');
+	} else if (playerDecision === computerChoice) {
+		alert('It is a tie');
+	} else if (playerDecision != 'rock' || 'paper' || 'scissors') {
+		alert('Invalid, do not tempt me human!')
+	}
+
+ } round();
+
+ function game() {
+	if (Humanity >= 5) {
+		alert('Game over, humanity is saved!');
+	} else if (AI >= 5) {
+		alert('Game over, humanity is doomed');
+	}
+  }
 
