c4b60685ffbc41f25d3b23a74bcac98b1444ebca
Nicholas <petree234@gmail.com>
2023-06-03 08:09:13 -0700

    Add pictures

--- RPS.js
+++ RPS.js
@@ -4,7 +4,7 @@
 let Humanity = 0;
 let AI = 0;
 
-//Create function to generate random computer choice 
+//Create function to generate random computer choice
 
 function getComputerChoice() { 
 
@@ -16,8 +16,6 @@ function getComputerChoice() {
  } getComputerChoice();
 
  //play a round of game
-
-
 
  function round() {
 	
@@ -55,20 +53,28 @@ function getComputerChoice() {
 
  } 
 
- document.getElementById("button").addEventListener("click", round())
+ let rock = document.getElementById('btn').addEventListener('click', round());
+
+ let scissors = document.getElementById('btn').addEventListener('click', round());
+
+ let paper = document.getElementById('btn').addEventListener('click', round());
 
 
  //Play game until one user gets to 5
 
- function game() {
+ //function game() {
 
-	if (Humanity >= 5) {
-		alert('Game over, Humanity is saved!')
-	} else if (AI >= 5) {
-		alert('Game over, Humanity is doomed!')
-	}
+	//if (Humanity >= 5) {
+	//	alert('Game over, Humanity is saved!')
+	//} else if (AI >= 5) {
+		//alert('Game over, Humanity is doomed!')
+	//}
 
-  } game();
+  //} game();
 
-  
+//1. For now, remove the logic that plays exactly five rounds.
+//2. Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+//3. Add a div for displaying results and change all of your console.logs into DOM methods.
+//4. Display the running score, and announce a winner of the game once one player reaches 5 points.
+//5. You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
 
